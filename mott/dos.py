6baebe1fd801960f16d927ba8ad52602dace9aae
henhans <henhans74716@gmail.com>
2024-08-09 19:18:05 +0800

    adapted simps to simpson

--- mott/dos.py
+++ mott/dos.py
@@ -4,8 +4,8 @@ def semicircle(x,t):
     return np.sqrt((2*t)**2-x**2)/(2*t**2*np.pi)
 
 if __name__=='__main__':
-    from scipy.integrate import simps
+    from scipy.integrate import simpson
 
     t = 0.5
     oms = np.linspace(-2*t,2*t,1000)
-    print(simps(semicircle(oms,t),oms))
+    print(simpson(semicircle(oms,t),oms))
